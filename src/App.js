2c195163b315c8bd85a2cc03c80c1e4d3552e060
Sensei <karox1241@wp.pl>
2019-04-29 16:57:38 +0200

    No loading anymore

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./layouts/Header";
 import Navbar from "./layouts/Navbar";
-import Content from "./layouts/Content"; 
+import Content from "./layouts/Content";
 import Footer from "./layouts/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
 
 class App extends Component {
   state = {
     isLogged: false,
-    isLoaded : true
+    isLoaded: true
   };
 
   handleLogin = () => {
@@ -19,51 +19,22 @@ class App extends Component {
       isLogged: !this.state.isLogged
     });
   };
- 
-
-  handleLoadingPage = () => {
-    this.setState({
-      isLoaded: true
-    });
-  };
-
-  componentDidMount() {
-    setTimeout(this.handleLoadingPage, 3000);
-  }
 
   render() {
     return (
-
-      <>
-      {this.state.isLoaded ?
-        (
-            <div className="container-fluid">
-              <div className="app">
-                <Router>
-                  <Header />
-                  <Navbar
-                    isLogged={this.state.isLogged}
-                    handleLogin={this.handleLogin}
-                  />
-                  <Content />
-                  <Footer />
-                </Router>
-              </div >
-            </div >
-           
-        ):(
-            <div id="loadingpage">
-              <div  class="spinner-border" role="status">
-              <span class="sr-only"/>
-              </div>
-            <div> Wczytywanie zasobów strony.
-                Strona stworzona przez dr Daniel Ochaba i kasztana
-            </div>
-            </div>
-             
-        ) 
-         }
-      </>
+      <div className="container-fluid">
+        <div className="app">
+          <Router>
+            <Header />
+            <Navbar
+              isLogged={this.state.isLogged}
+              handleLogin={this.handleLogin}
+            />
+            <Content />
+            <Footer />
+          </Router>
+        </div>
+      </div>
     );
   }
 }
