d819262f5de5b7ceb87ba6899b4838606c27c4a9
Sensei <karox1241@wp.pl>
2019-04-29 18:47:42 +0200

    Updates

---
+++ precache-manifest.0c9b5589b1a9dfd23399c528e8ba9bb9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac58f683d10d8a2d2db1",
+    "url": "/ProjektAI/static/css/main.83f44ea9.chunk.css"
+  },
+  {
+    "revision": "ac58f683d10d8a2d2db1",
+    "url": "/ProjektAI/static/js/main.920dee35.chunk.js"
+  },
+  {
+    "revision": "cfbc8cb4a6b0c1d14181",
+    "url": "/ProjektAI/static/js/runtime~main.3f285973.js"
+  },
+  {
+    "revision": "7a68354e9220dca14617",
+    "url": "/ProjektAI/static/css/2.b2c31f6a.chunk.css"
+  },
+  {
+    "revision": "7a68354e9220dca14617",
+    "url": "/ProjektAI/static/js/2.670f7ad8.chunk.js"
+  },
+  {
+    "revision": "5bf75d7930f451069f2f195959f4cf5a",
+    "url": "/ProjektAI/static/media/home1.5bf75d79.jpg"
+  },
+  {
+    "revision": "e09973b3ccb58f3ec5187da401069dc0",
+    "url": "/ProjektAI/static/media/home2.e09973b3.jpg"
+  },
+  {
+    "revision": "46ed16685520bb61230a197b7ef309ee",
+    "url": "/ProjektAI/index.html"
+  }
+];
