133eae5fd73e0b8738a79930c7416efa1f90bfee
Sensei <karox1241@wp.pl>
2019-05-11 15:55:24 +0200

    register, rules

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import "../styles/register.css";
 import { Link } from "react-router-dom";
+import "../styles/rules.css";
+
 class RegisterPage extends Component {
   state = {
     username: "",
     password: "",
     email: "",
-    agree: false
+    agree: false,
+    name: "",
+    lastname: "",
+    city: "",
+    street: ""
   };
 
   handleChange = e => {
@@ -26,13 +32,26 @@ class RegisterPage extends Component {
   validation = () => {
     let correct = false;
 
-    const { username, email, password, agree } = this.state;
+    const {
+      username,
+      email,
+      password,
+      agree,
+      name,
+      lastname,
+      city,
+      street
+    } = this.state;
 
     if (
-      username.length > 3 &&
-      password.length > 5 &&
+      username.length > 2 &&
+      password.length > 4 &&
       agree === true &&
-      email.length > 3
+      email.length > 2 &&
+      name.length > 2 &&
+      lastname.length > 2 &&
+      city.length > 2 &&
+      street.length > 2
     )
       correct = true;
 
@@ -47,6 +66,8 @@ class RegisterPage extends Component {
     } else {
       alert("Formularz został błędnie wypełniony");
     }
+
+    console.log(this.state);
   };
 
   render() {
@@ -54,35 +75,80 @@ class RegisterPage extends Component {
       <div className="register">
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
+            <label>Nazwa użytkownika</label>
             <input
               className="form-control"
               type="text"
               value={this.state.username}
-              placeholder="Nazwa użytkownika"
               onChange={this.handleChange}
               name="username"
               minLength="3"
             />
           </div>
           <div className="form-group">
+            <label> Hasło </label>
             <input
               className="form-control"
               type="password"
               value={this.state.password}
-              placeholder="Hasło"
               onChange={this.handleChange}
               name="password"
               minLength="5"
             />
           </div>
           <div className="form-group">
+            <label>Adres e-mail</label>
             <input
               className="form-control"
               type="email"
               value={this.state.email}
-              placeholder="E-mail"
               onChange={this.handleChange}
               name="email"
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Imię</label>
+            <input
+              className="form-control"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+              name="name"
+              minLength="3"
+            />
+          </div>
+          <div className="form-group">
+            <label> Nazwisko </label>
+            <input
+              className="form-control"
+              type="text"
+              value={this.state.lastname}
+              onChange={this.handleChange}
+              name="lastname"
+              minLength="3"
+            />
+          </div>
+          <div className="form-group">
+            <label> Miasto </label>
+            <input
+              className="form-control"
+              type="text"
+              value={this.state.city}
+              onChange={this.handleChange}
+              name="city"
+              minLength="3"
+            />
+          </div>
+          <div className="form-group">
+            <label> Ulica </label>
+            <input
+              className="form-control"
+              type="text"
+              value={this.state.street}
+              onChange={this.handleChange}
+              name="street"
+              minLength="3"
             />
           </div>
           <input
@@ -91,8 +157,12 @@ class RegisterPage extends Component {
             onChange={this.handleChange}
             name="agree"
           />
+
           <span>
-            Oświadczam, że zapoznałem się z <Link to="/rules">Regulaminem</Link>
+            Oświadczam, że zapoznałem się z{" "}
+            <Link data-toggle="modal" data-target="#rules">
+              Regulaminem{" "}
+            </Link>
             , akceptuję jego treść i zobowiązuję się przestrzegać jego
             postanowień.
           </span>
@@ -102,6 +172,236 @@ class RegisterPage extends Component {
             </button>
           </div>
         </form>
+
+        <div className="modal" id="rules">
+          <div className="modal-dialog modal-lg">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h2>Regulamin</h2>
+                <button type="button" className="close" data-dismiss="modal">
+                  &times;
+                </button>
+              </div>
+
+              <div className="modal-body">
+                <ol type="I">
+                  <li> Informacje Ogólne</li>
+                  <ol type="1">
+                    <li>
+                      Klienci mogą uzyskać dostęp do niniejszego Regulaminu w
+                      każdym czasie za pośrednictwem odsyłacza zamieszczonego na
+                      stronie głównej serwisu www.musclepower.pl
+                    </li>
+                    <li>
+                      Wszelkie treści jak i zapisy niniejszego regulaminu są
+                      objęte ochroną prawami autorskimi i stanowią własność
+                      Muscle Power. Kopiowanie, rozpowszechnianie i jakiekolwiek
+                      wykorzystanie bez zgody autora są zabronione. Naruszenie
+                      powyższego stanowi jednoznaczną, dobrowolną akceptację
+                      poddania się karze i wypłacenia kary umownej w wysokości
+                      25 000 zł co nie zamyka roszczeń właściciela praw
+                      autorskich z tego tytułu i pozostawia możliwość dalszego
+                      dochodzenia odszkodowań na drodze sądowej.
+                    </li>
+                  </ol>
+                  <li>OFERTA</li>
+                  <ol type="1">
+                    <li>
+                      {" "}
+                      Produkty, które Muscle Power prezentuje na stronie sklepu,
+                      stanowią zaproszenie do zawarcia umowy w myśl art.71
+                      kodeksu cywilnego. Muscle Power zobowiązuje się, że
+                      prezentowane produkty będą dostarczone bez wad. Muscle
+                      Power dokłada wszelkich starań aby prezentowane
+                      dostępności towarów, opisy i zdjęcia przedstawione na
+                      stronie rzetelnie odwzorowywały wygląd produktów, jednakże
+                      mają one jedynie charakter poglądowy i mogą nieznacznie
+                      różnić się od towarów dostarczanych przez producentów,
+                      oraz faktyczną dostepnością towarów.
+                    </li>
+                    <li>
+                      Wszystkie prezentowane ceny towarów są proponowanymi
+                      cenami brutto (zawierają podatek VAT), podane są w złotych
+                      polskich. Cena staje się wiążąca w chwili złożenia
+                      zamówienia.
+                    </li>
+                    <li>
+                      W przypadku braku zamówionego towaru na magazynie, Klient
+                      jest informowany o tym fakcie telefonicznie lub na adres
+                      e-mail wskazany w Zamówieniu i ma możliwość wyboru między:
+                      zmianą kupowanego towaru na inny, dłuższym czasem
+                      realizacji lub rezygnacją z zamówienia.
+                    </li>
+                    <li>
+                      Zgodnie z obowiązującymi przepisami prawa Sprzedawca
+                      zastrzega sobie możliwość ograniczenia świadczenia usług
+                      za pośrednictwem Sklepu internetowego do osób, które
+                      ukończyły wiek 18 lat. W takim przypadku potencjalni
+                      Klienci zostaną o powyższym powiadomieni.
+                    </li>
+                  </ol>
+                  <li> REALIZACJA ZAMÓWIEŃ i WYSYŁKA TOWARU</li>
+                  <ol type="1">
+                    <li>
+                      W celu złożenia zamówienia Kupujący zobowiązany jest
+                      zarejestrować się w panelu Sklepu podając prawdziwe dane
+                      osobowe/firmowe i akceptując niniejszy Regulamin,
+                      prawidłowo wypełnić formularz zamówienia, podając dane
+                      niezbędne do realizacji wysyłki.{" "}
+                    </li>
+                    <li>
+                      Potwierdzenie prawidłowo złożonego zamówienia wysyłane
+                      jest automatycznie drogą mailową na adres podany przez
+                      Kupującego. Tą samą drogą Klient może być powiadamiany o
+                      statusie realizacji zamówienia. W przypadkach szczególnych
+                      Sprzedawca zastrzega sobie możliwość kontaktu z Klientem
+                      celem ustalenia szczegółów realizacji zamówienia.
+                    </li>
+                    <li>
+                      Zamówienia są realizowane w dni robocze, w godzinach pracy
+                      Sprzedającego i wysyłane od 2 do 7 dni roboczych. O
+                      przypadkach szczególnych mogących potencjalnie wydłużyć
+                      termin realizacji Klient jest informowany przez Sprzedawcę
+                      wraz z pouczeniem o przysługujących mu prawach.
+                    </li>
+                    <li>
+                      W zamówieniach, w których Kupujący wybrał jako formę
+                      płatności przedpłatę na konto Sprzedawcy, realizacja
+                      zamówienia rozpoczyna się z momentem zaksięgowania
+                      przesłanych środków pieniężnych na konto bankowe Muscle
+                      Power.
+                    </li>
+                    <li>
+                      Po dokonaniu wysyłki zakupionego towaru, Sklep w ramach
+                      realizacji umowy z Kupującym jest zobowiązany przesłać
+                      Kupującemu na adres poczty elektronicznej zaproszenie do
+                      wypełnienia ankiety w celu zbadania jego opinii o
+                      przeprowadzonej transakcji. Kupujący jest uprawniony,
+                      jednak nie zobowiązany do jej wypełnienia. Sklep zastrzega
+                      sobie prawo do zatwierdzania opinii zgodnych z zakresem
+                      przeprowadzonej transakcji i produktów z niej
+                      wynikających. Sklep zastrzega sobie prawo do
+                      niezatwierdzenia opinii niezgodnej z zapisami regulaminu,
+                      stanem faktycznym wynikającym z historii realizacji
+                      zamówienia w systemie Sklepu, opinii kontrowersyjnych czy
+                      naruszających prawo. Ocena opinii pozostaje w wyłącznej
+                      gestii Sklepu. Za zatwierdzoną opinię Kupujący uzyskuje
+                      punkty lojalnościowe zgodnie z zapisami Programu
+                      lojalnościowego.
+                    </li>
+                    <li>
+                      Wybierając produkt w Sklepie z podanym wariantem (tj.
+                      smak, kolor, rozmiar, model) Klient akceptuje fakt, że nie
+                      ma możliwości zamiany na inny, otrzyma produkt który
+                      zatwierdził w zamówieniu. Nie ma możliwości zmiany
+                      produktów czy jego wariantów na podstawie uwag w
+                      komentarzu czy na podstawie wiadomości e-mail. W przypadku
+                      produktów oznaczonych jako gratisy Klient akceptuje fakt,
+                      że są one nadawane przez system automatycznie w losowym
+                      wariancie, rozmiarze i kolorze. Nie ma możliwości wymiany
+                      gratisów uzyskanych po osiągnięciu progu cenowego, tylko
+                      takie jak zostały przyznane automatycznie przez system
+                      Sprzedającego. Adres do wysyłki Klient uzupełnia w
+                      formularzu dostępnym przy składaniu zamówienia, adresy
+                      podawane w innych miejscach(komentarze, uwagi itp.) nie
+                      będą brane pod uwagę. Jeżeli Klient pomylił się podając
+                      adres powinien o tym fakcie poinformować Sprzedającego bez
+                      zbędnej zwłoki, przed wysyłką towaru poprzez kontakt
+                      osobisty, telefoniczny bądź mailowy.
+                    </li>
+                    <li>
+                      Zawierając umowę kupna ze Sprzedającym Klient zobowiązuje
+                      się do jej przestrzegania. W przypadku nie odebrania
+                      przesyłki z zakupionym towarem, Klienta nadal obowiązuje
+                      zawarta umowa a dodatkowo zobowiązany jest do pokrycia
+                      kosztów odesłania towaru i jego ewentualnej powtórnej
+                      wysyłki. Nie dotyczy to przypadku odstąpienia od umowy
+                      (par. V Regulaminu ) jak i przypadku opisanego w par. III.
+                      pkt 9 Regulaminu.
+                    </li>
+                  </ol>
+                  <li>Gwarancje i reklamacje</li>
+                  <ol type="1">
+                    <li>
+                      Muscle Power oświadcza iż dokłada wszelkich starań w celu
+                      zapewnienia zgodność z opisem towarów sprzedanych przez
+                      siebie na stronie internetowej musclepower.pl
+                    </li>
+                    <li>
+                      Muscle Power jako Sprzedawca odpowiada wobec Klienta
+                      będącego Konsumentem w rozumieniu art. 22 Kodeksu
+                      Cywilnego, z tytułu rękojmi za wady w zakresie określonym
+                      w Kodeksie Cywilnym, w szczególności w art. 556 oraz art.
+                      556 - 556 i kolejnych Kodeksu Cywilnego.
+                    </li>
+                    <li>
+                      Reklamacje, wynikające z naruszenia praw gwarantowanych
+                      ustawowo, lub na podstawie niniejszego Regulaminu, należy
+                      kierować na adres sklep@musclepower.pl. Firma Muscle Power
+                      zobowiązuje się do rozpatrzenia każdej reklamacji w
+                      terminie do 30 dni. W szczególnie skomplikowanych sprawach
+                      Klient może zostać poinformowany o wydłużeniu rozpatrzenia
+                      Reklamacji jednak nie dłużej niż 60 dni od dnia
+                      zgłoszenia. Nierozpatrzenie reklamacji w powyższym
+                      terminie oznacza uznanie jej za uzasadnioną.
+                    </li>
+                    <li>
+                      Muscle Power oświadcza, że nie jest producentem towarów.
+                      Producent ponosi odpowiedzialność z tytułu gwarancji
+                      sprzedanego Towaru na warunkach oraz przez okres wskazany
+                      w karcie gwarancyjnej, opisie produktu lub określony
+                      przepisami prawa(jeśli oznaczenia takowego nie ma). Jeśli
+                      dokument gwarancyjny/ opis produktu przewiduje taką
+                      możliwość, Klient może zgłaszać swoje roszczenia w ramach
+                      gwarancji bezpośrednio u Producenta, którego adres
+                      znajduje się w karcie gwarancyjnej/oznaczeniu towaru.
+                      Gwarancja nie wyłącza, nie zawiesza, nie ogranicza
+                      uprawnień kupującego z tytułu niezgodności towaru
+                      konsumpcyjnego z umową.
+                    </li>
+                    <li>
+                      Muscle Power podejmuje działania w celu zapewnienia w
+                      pełni poprawnego działania Sklepu, w takim zakresie, jaki
+                      wynika z aktualnej wiedzy technicznej i zobowiązuje się
+                      usunąć w rozsądnym terminie wszelkie nieprawidłowości
+                      zgłoszone przez Klientów. Zgłoszeń Klienci mogą dokonywać
+                      w formie pisemnej na adres korespondencyjny Muscle Power
+                      lub poprzez e-mail na adres sklep@musclepower.pl
+                    </li>
+                  </ol>
+                  <li> Roszczenia sporne</li>
+                  <ol type="1">
+                    <li>
+                      Rozstrzyganie sporów powstałych pomiędzy firmą Muscle
+                      Power a Klientem, który jest Konsumentem w rozumieniu art.
+                      221 Kodeksu Cywilnego, zostaje przekazane sądom właściwym
+                      zgodnie z zapisami KPC.
+                    </li>
+                    <li>
+                      Rozstrzyganie ewentualnych sporów powstałych pomiędzy
+                      firmą Muscle Power a Klientem, który nie jest konsumentem
+                      w rozumieniu art. 221 KC, zostaje skierowane do Sądu
+                      właściwego ze względu na siedzibę firmy Muscle Power.
+                    </li>
+                    <li>
+                      W przypadku, gdy Klient będzie chciał wdać się w spór z
+                      Muscle Power w związku z obsługą reklamacji towaru ma
+                      również możliwość skorzystania z dostępnych metod
+                      rozstrzygania sporów na jakie Muscle Power wyrazi zgodę w
+                      pismach obsługi reklamacji.
+                    </li>
+                    <li>
+                      W sprawach nieuregulowanych w niniejszym Regulaminie mają
+                      zastosowanie przepisy Kodeksu cywilnego, przepisy Ustawy o
+                      świadczeniu usług drogą elektroniczną oraz inne właściwe
+                      przepisy prawa polskiego.
+                    </li>
+                  </ol>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
